c66df9cca6050b5e3ae638a6ccdc43fd94619a05
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-01 17:47:18 -0600

    Department Microservice Database configured with required packages

---
+++ department-service/src/main/java/com/microservice/departmentservice/entity/Department.java
@@ -0,0 +1,22 @@
+package com.microservice.departmentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+
+}
